413513e36a03b5ba0a6c0f71643dfe2738aa56f7
cc3 <cc3@10.2.80.94>
2017-05-26 23:14:15 +0300

    intialised

---
+++ src/personal/information/PersonalInformation.java
@@ -0,0 +1,41 @@
+package personal.information;
+
+import java.util.Scanner;
+
+public class PersonalInformation {
+
+    String FirstName;
+    String LastName;
+    String sex;
+    int age;
+    String MaritialStatus;
+    String AcadamicStatus;
+
+    public void SetpersonalInfo() {
+        System.out.println("insert info \n");
+        Scanner sn = new Scanner(System.in);
+        FirstName = sn.next();
+        LastName = sn.next();
+        sex = sn.next();
+        age = sn.nextInt();
+
+    }
+
+    public void DisplaypersonalInfo() {
+        System.out.println("here is the info");
+        System.out.println("Fname" + " " + FirstName);
+        System.out.println("Lname" + " " + LastName);
+        System.out.println("sex" + " " + sex);
+        System.out.println("age" + " " + age);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        PersonalInformation pi = new PersonalInformation();
+        pi.SetpersonalInfo();
+        pi.DisplaypersonalInfo();
+
+    }
+
+}
